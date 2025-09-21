6318d3978e94848bb00f90bffa318f4193c7e4d7
Mamun123123123 <almalm90459@gmail.com>
2025-09-21 12:03:47 +0600

    new

---
+++ j.js
@@ -0,0 +1,13 @@
+let arr = [2,1,3,4,5]
+for(let ar of arr) console.log(ar);
+arr.push(6)
+ console.log(arr)   
+ arr.unshift(0)
+ console.log(arr)   
+arr.shift()
+arr.pop()
+console.log(arr)   
+
+arr.splice(2,0,"mamun")
+ console.log(arr)   
+
